a2248c1fc5af05c8cad174acb6735b27ab7d858f
coiEscamilla <89111204+AEO-esc@users.noreply.github.com>
2022-11-20 21:49:53 -0600

    Create CharBoolean

---
+++ java/charBoolean.java
@@ -0,0 +1,8 @@
+public class charBoolean {
+    public static void main(String[] args){
+    char myChar = 'D';
+    char myUnicodeChar = '\u0044';
+    System.out.println(myChar);
+    System.out.println(myUnicodeChar);
+    }
+}
